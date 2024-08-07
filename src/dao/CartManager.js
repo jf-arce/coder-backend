823b32fc872601cac62eadd21817610ed0c83aae
jf-arce <josefranciscoarce21@gmail.com>
2024-08-06 23:14:21 -0300

    add carts routes

--- src/dao/CartManager.js
+++ src/dao/CartManager.js
@@ -1,28 +1,68 @@
 import fs from "fs";
-import crypto from "crypto"; 
+import crypto from "crypto";
 
 export class CartManager {
-    static path; 
+    static path;
 
     static async createCart() {
-        if (!this.path) {
-            throw new Error("Path is not defined");
-        }
-
         if (fs.existsSync(this.path)) {
             const cart = {
                 id: crypto.randomUUID(),
-                cart: [] 
+                products: [],
             };
-
             const cartBD = JSON.parse(await fs.promises.readFile(this.path, { encoding: "utf-8" }));
-
             cartBD.push(cart);
 
-            await fs.promises.writeFile(this.path, JSON.stringify(cartBD, null, 2), { encoding: "utf-8" });
-        } else {
+            await fs.promises.writeFile(this.path, JSON.stringify(cartBD, null, 2), {
+                encoding: "utf-8",
+            });
 
-            throw new Error(`File at path ${this.path} does not exist`);
+            return cart;
+        } else {
+            throw new Error(`El archivo en el path ${this.path} no existe`);
+        }
+    }
+
+    static async getCartProducts(cid) {
+        if (fs.existsSync(this.path)) {
+            const carts = JSON.parse(await fs.promises.readFile(this.path, { encoding: "utf-8" }));
+            const cartFound = carts.find((cart) => cart.id === cid);
+            if (!cartFound) throw new Error(`No se encontro el carrito con el id ${cid}`);
+            return cartFound.products;
+        } else {
+            throw new Error(`El archivo en el path ${this.path} no existe`);
+        }
+    }
+
+    static async addProductToCart(cid, pid) {
+        if (fs.existsSync(this.path)) {
+            const carts = JSON.parse(await fs.promises.readFile(this.path, { encoding: "utf-8" }));
+            const cartFound = carts.find((cart) => cart.id === cid);
+            if (!cartFound) {
+                throw new Error("Cart not found");
+            }
+            const productExist = cartFound.products.some((prod) => prod.id === pid);
+            let newCart;
+            if (productExist) {
+                newCart = {
+                    ...cartFound,
+                    products: cartFound.products.map((prod) =>
+                        prod.id === pid ? { ...prod, quantity: prod.quantity + 1 } : prod,
+                    ),
+                };
+            } else {
+                newCart = {
+                    ...cartFound,
+                    products: [...cartFound.products, { id: pid, quantity: 1 }],
+                };
+            }
+
+            const newCartsArray = carts.map((cart) => (cart.id === newCart.id ? newCart : cart));
+            await fs.promises.writeFile(this.path, JSON.stringify(newCartsArray, null, 2), {
+                encoding: "utf-8",
+            });
+        } else {
+            throw new Error(`El archivo en el path ${this.path} no existe`);
         }
     }
 }
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,14 +1,56 @@
 import { Router } from "express";
 import { CartManager } from "../dao/CartManager.js";
 
-export const cartsRouter = Router()
+export const cartsRouter = Router();
 
-cartsRouter.post('/',async (req,res)=>{
-    try{
-        await CartManager.createCart();
-        res.status(201).json({message: "Carrito creado con exito"}, );
-    }catch(error){
-        console.log("Error en crear en el carrito")
+cartsRouter.post("/", async (req, res) => {
+    try {
+        const cart = await CartManager.createCart();
+        res.setHeader("Content-Type", "application/json");
+        res.status(201).json({ message: "Carrito creado con exito", cart });
+    } catch (error) {
+        console.log(error);
+        res.setHeader("Content-Type", "application/json");
+        return res.status(500).json({
+            error: "Error inesperado en el servidor",
+            detalle: `${error.message}`,
+        });
     }
+});
 
+cartsRouter.get("/:cid", async (req, res) => {
+    const { cid } = req.params;
+    if (!cid) return res.status(400).json({ error: "No se paso el id del carrito" });
+
+    try {
+        const products = await CartManager.getCartProducts(cid);
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error);
+        res.setHeader("Content-Type", "application/json");
+        return res.status(500).json({
+            error: "Error inesperado en el servidor",
+            detalle: `${error.message}`,
+        });
+    }
+});
+
+cartsRouter.post("/:cid/products/:pid", async (req,res) => {
+    const {cid, pid } = req.params;
+
+    if (!cid || !pid) return res.status(400).json({error: "Tienen que pasarse todos los parametros obligatorios"})
+
+    try{
+        await CartManager.addProductToCart(cid,pid);
+        res.setHeader("Content-Type", "application/json")
+        res.status(200).json({message: "Producto agregado correctamente"})
+    }catch(error){
+        console.log(error)
+        res.setHeader("Content-Type", "application/json");
+        return res.status(500).json({
+            error: "Error inesperado en el servidor",
+            detalle: `${error.message}`
+        })
+    }
 })
