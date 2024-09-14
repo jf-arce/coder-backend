95d2e792b04eac981c441562cd3eaf60677cf989
jf-arce <josefranciscoarce21@gmail.com>
2024-09-14 18:49:45 -0300

    Entrega Final

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ## Curso Backend 1 Coderhouse
-Preentrega 1
+Trabajo Final
--- public/js/home.js
+++ public/js/home.js
@@ -3,6 +3,10 @@ const currentPage = document.getElementById("current-page");
 const btnNext = document.getElementById("btn-next");
 const btnPrev = document.getElementById("btn-prev");
 const cartView = document.getElementById("cart-view");
+const btnFirst = document.getElementById("btn-first");
+const btnLast = document.getElementById("btn-last");
+const totalPages = document.getElementById("total-pages");
+const cleanFilters = document.getElementById("clean-filters");  
 
 let maxPage;
 let currentPageNumber;
@@ -12,13 +16,28 @@ const getProducts = async () => {
     try {
         const params = new URLSearchParams(location.search);
         let page = params.get("page");
+        let limit = params.get("limit");
+        let sort = params.get("sort");
+        let query = params.get("query");  
+        
         if (!page || isNaN(Number(page))) {
             page = 1;
         }
-        const res = await fetch(`/api/products/?page=${page}`);
+        if (!limit || isNaN(Number(limit))) {
+            limit = 10;
+        }
+        if (!sort) {
+            sort = "";
+        }   
+        if (!query) {
+            query = "";
+        }
+
+        const res = await fetch(`/api/products/?page=${page}&limit=${limit}&sort=${sort}&query=${query}`);
         const data = await res.json();
 
         maxPage = data.totalPages;
+        totalPages.innerHTML = maxPage;
         currentPageNumber = data.page;
 
         containerHome.innerHTML = "";
@@ -38,7 +57,7 @@ const getProducts = async () => {
             <div class="card-content">
                 <h3 class="title-product">${prod.title}</h3>
                 <p class="description-product">${prod.description}</p>
-                <p class="price-product">${prod.price}</p>
+                <p class="price-product">$${prod.price}</p>
             </div>
             <footer class="card-footer">
                 <button id="btn-addToCart" class="btn-addToCart" data-id=${prod.id}>Agregar al carrito</button>
@@ -52,9 +71,13 @@ const getProducts = async () => {
     }
 };
 
-//Paginación
+/**************************** PAGINACION ******************************/
 const params = new URLSearchParams(location.search);
 let page = Number(params.get('page'));
+let category = params.get('query');
+let sort = params.get('sort');
+let limit = params.get('limit');
+
 if (!page || isNaN(Number(page))) {
     page = 1;
 }
@@ -79,30 +102,99 @@ getProducts().then(() => {
 //Redireccionar a la siguiente pagina
 btnNext.addEventListener('click', async () => {
     page++;
-    location.href = `/products?page=${page}`;
+    const queryString = `page=${page}${category ? `&query=${category}` : ''}${sort ? `&sort=${sort}` : ''}${limit ? `&limit=${limit}` : ''}`;
+    location.href = `/products?${queryString}`
 });
 
 //Redireccionar a la pagina anterior
 btnPrev.addEventListener('click', async () => {
     page--;
-    location.href = `/products?page=${page}`;
+    const queryString = `page=${page}${category ? `&query=${category}` : ''}${sort ? `&sort=${sort}` : ''}${limit ? `&limit=${limit}` : ''}`;
+    location.href = `/products?${queryString}`
 });
 
-//CART
+//Redireccionar a la primera pagina
+btnFirst.addEventListener('click', async () => {
+    page = 1;
+    const queryString = `page=${page}${category ? `&query=${category}` : ''}${sort ? `&sort=${sort}` : ''}${limit ? `&limit=${limit}` : ''}`;
+    location.href = `/products?${queryString}`
+});
+
+//Redireccionar a la ultima pagina
+btnLast.addEventListener('click', async () => {
+    page = maxPage;
+    const queryString = `page=${page}${category ? `&query=${category}` : ''}${sort ? `&sort=${sort}` : ''}${limit ? `&limit=${limit}` : ''}`;
+    location.href = `/products?${queryString}`
+});
+
+/******************************** CART ******************************************/
 const CART = "66e4801c7f809369afe2de10"
 
 cartView.addEventListener("click", async () => {
     location.href = `/cart`;
 });
 
+
+//Agregar producto al carrito
 containerHome.addEventListener("click", async (e) => {
     if (e.target.classList.contains("btn-addToCart")) {
         const id = e.target.dataset.id;
         
         await fetch(`/api/carts/${CART}/products/${id}`, {
             method: "POST",
+        }).then(() => {
+            Swal.fire({
+                title: 'Agregado al carrito!',
+                text: 'Continúa comprando o ve al carrito para ver tu compra',
+                icon: 'success',
+                confirmButtonText: 'Ok'
+            }).then(() => {
+                location.reload();
+            }); 
         });
     }
 });
 
 
+/********************* Filters ****************** */
+const filtersForm = document.getElementById("filters-form");
+
+filtersForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // Crear un objeto FormData con los valores del formulario
+    const formData = new FormData(e.target);
+
+    const category = formData.get('category');
+    const sort = formData.get('sort');
+    const limit = formData.get('limit');
+
+    const params = new URLSearchParams();
+
+    if (category !== 'all') params.append('query', category);  //Si category es all mostrar todos los productos
+    if (sort) params.append('sort', sort);
+    if (limit) params.append('limit', limit);
+
+    const queryString = params.toString();
+    const url = `/products?${queryString}`;
+
+    location.href = url;
+});
+
+
+// Al cargar la página, restaurar los filtros desde la URL
+document.addEventListener("DOMContentLoaded", () => {
+    const params = new URLSearchParams(window.location.search);
+
+    const category = params.get('query');
+    const sort = params.get('sort');
+    const limit = params.get('limit');
+
+    if (category) document.getElementById('category-filter').value = category;
+    if (sort) document.getElementById('price-filter').value = sort;
+    if (limit) document.getElementById('limit-filter').value = limit;
+});
+
+//Limpiar filtros
+cleanFilters.addEventListener("click", () => {
+    location.href = `/products`;
+});
